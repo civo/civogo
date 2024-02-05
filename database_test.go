a2e98fcc79d00f7d83ae6a179f3f3cddca6416a7
Uzair Ali <uzaxirr@gmail.com>
2024-02-06 04:05:05 +0530

    Increased test coverage for database.go

--- database_test.go
+++ database_test.go
@@ -108,3 +108,149 @@ func TestDeleteDatabase(t *testing.T) {
 		t.Errorf("Expected %+v, got %+v", expected, got)
 	}
 }
+
+func TestRestoreDatabase(t *testing.T) {
+	client, server, _ := NewClientForTesting(map[string]string{
+		"/v2/databases/12345/restore": `{"result": "success"}`,
+	})
+	defer server.Close()
+
+	// Define the input parameters for RestoreDatabase
+	restoreRequest := &RestoreDatabaseRequest{
+		Name:      "test-db",
+		Software:  "MySQL",
+		NetworkID: "network-id",
+		Backup:    "backup-file-path",
+		Region:    "some-region",
+	}
+
+	// Call RestoreDatabase method
+	got, err := client.RestoreDatabase("12345", restoreRequest)
+	if err != nil {
+		t.Errorf("RestoreDatabase returned an error: %s", err)
+		return
+	}
+
+	expected := &SimpleResponse{Result: "success"}
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected %+v, got %+v", expected, got)
+	}
+}
+
+func TestGetDatabase(t *testing.T) {
+	client, server, _ := NewClientForTesting(map[string]string{
+		"/v2/databases/12345": `{
+			"id": "12345",
+			"name": "test-db",
+			"nodes": 1,
+			"size": "g3.db.small",
+			"software": "MySQL",
+			"software_version": "8.0",
+			"public_ipv4": "127.0.0.1",
+			"network_id": "network-id",
+			"firewall_id": "firewall-id",
+			"port": 3306,
+			"username": "user",
+			"password": "password",
+			"database_user_info": [
+				{"username": "user", "password": "password", "port": 3306}
+			],
+			"status": "Ready"
+		}`,
+	})
+	defer server.Close()
+
+	// Call GetDatabase method
+	got, err := client.GetDatabase("12345")
+	if err != nil {
+		t.Errorf("GetDatabase returned an error: %s", err)
+		return
+	}
+
+	expected := &Database{
+		ID:              "12345",
+		Name:            "test-db",
+		Nodes:           1,
+		Size:            "g3.db.small",
+		Software:        "MySQL",
+		SoftwareVersion: "8.0",
+		PublicIPv4:      "127.0.0.1",
+		NetworkID:       "network-id",
+		FirewallID:      "firewall-id",
+		Port:            3306,
+		Username:        "user",
+		Password:        "password",
+		DatabaseUserInfo: []DatabaseUserInfo{
+			{Username: "user", Password: "password", Port: 3306},
+		},
+		Status: "Ready",
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected %+v, got %+v", expected, got)
+	}
+}
+
+func TestUpdateDatabase(t *testing.T) {
+	client, server, _ := NewClientForTesting(map[string]string{
+		"/v2/databases/12345": `{
+			"id": "12345",
+			"name": "updated-test-db",
+			"nodes": 2,
+			"size": "g3.db.medium",
+			"software": "PostgreSQL",
+			"software_version": "13",
+			"public_ipv4": "127.0.0.1",
+			"network_id": "network-id",
+			"firewall_id": "firewall-id",
+			"port": 5432,
+			"username": "user",
+			"password": "password",
+			"database_user_info": [
+				{"username": "user", "password": "password", "port": 5432}
+			],
+			"status": "Ready"
+		}`,
+	})
+	defer server.Close()
+
+	nodeCount := 2
+
+	// Define the input parameters for UpdateDatabase
+	updateRequest := &UpdateDatabaseRequest{
+		Name:       "updated-test-db",
+		Nodes:      &nodeCount, // Int is a helper function to create an integer pointer
+		FirewallID: "firewall-id",
+		Region:     "region",
+	}
+
+	// Call UpdateDatabase method
+	got, err := client.UpdateDatabase("12345", updateRequest)
+	if err != nil {
+		t.Errorf("UpdateDatabase returned an error: %s", err)
+		return
+	}
+
+	expected := &Database{
+		ID:              "12345",
+		Name:            "updated-test-db",
+		Nodes:           2,
+		Size:            "g3.db.medium",
+		Software:        "PostgreSQL",
+		SoftwareVersion: "13",
+		PublicIPv4:      "127.0.0.1",
+		NetworkID:       "network-id",
+		FirewallID:      "firewall-id",
+		Port:            5432,
+		Username:        "user",
+		Password:        "password",
+		DatabaseUserInfo: []DatabaseUserInfo{
+			{Username: "user", Password: "password", Port: 5432},
+		},
+		Status: "Ready",
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected %+v, got %+v", expected, got)
+	}
+}
