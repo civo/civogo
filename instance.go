31e530008ebd947ae8dad2a2717ee7d9512d9f6f
david.woolger@civo.com <david.woolger@civo.com>
2025-05-21 16:11:44 +0100

    Corrected comment

--- instance.go
+++ instance.go
@@ -469,7 +469,7 @@ func (c *Client) UpdateInstanceAllowedIPs(id string, allowedIPs []string) (*Simp
 	return c.DecodeSimpleResponse(resp)
 }
 
-// UpdateInstanceAllowedIPs sets the list of IP addresses that an instance is allowed to use
+// UpdateInstanceBandwidth sets the list of IP addresses that an instance is allowed to use
 func (c *Client) UpdateInstanceBandwidth(id string, bandwidthLimit int) (*SimpleResponse, error) {
 	resp, err := c.SendPutRequest(fmt.Sprintf("/v2/instances/%s/network_bandwidth_limit", id), bandwidthLimit)
 	if err != nil {
